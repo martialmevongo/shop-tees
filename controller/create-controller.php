eeee08b86ce9dd4bb2edc6ebf73f1a90ecdd0d84
martialmevongo <martial.mevongo@lapiscine.pro>
2025-03-25 16:12:13 +0100

    get order values

--- controller/create-controller.php
+++ controller/create-controller.php
@@ -1,3 +1,20 @@
 <?php
 // je recupère le fichier create view
 require_once('../view/create-view.php');
+
+//je créé une function qui recupère le valeurs une fois qu'elles sont envoyées
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // je recupère les valeurs de vêtement et leurs tailles
+    $vetement = $_POST['vetement'];
+    $taille = $_POST['taille'];
+
+    // Afficher les choix de l'utilisateur
+    echo "<h1>Merci pour votre choix !</h1>";
+    echo "<p>Vous avez choisi un <strong>" . htmlspecialchars($vetement) . "</strong> en taille <strong>" . htmlspecialchars($taille) . "</strong>.</p>";
+} else {
+    echo "<p>Il n'y a pas de données soumises.</p>";
+}
+
+
+//je recupère le fichier view config
+require_once('../view/config.php');
--- view/create-view.php
+++ view/create-view.php
@@ -6,8 +6,29 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="">
-        <label><input type="text">quantity</label>
+    <header><h1>formulaire de selection</h1></header>
+    <main>
+    <form method="POST">
+        
+        <label for="vetement">Choisissez un Vêtement:</label>
+        <select name="vetement" id="vetement" required>
+            <option value="T-shirt">T-shirt</option>
+            <option value="Pantalon">Pantalon</option>
+            <option value="Veste">Veste</option>
+            <option value="Robe">Robe</option>
+        </select><br><br>
+       
+        <label for="taille">Choisissez une Taille:</label>
+        <select name="taille" id="taille" required>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+            <option value="XXL">XXL</option>
+        </select><br><br>
+    
+        <input type="submit" value="Envoyer">
     </form>
+    </main>
 </body>
 </html>
