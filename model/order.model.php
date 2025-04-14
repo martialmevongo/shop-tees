90b33d796684ab16656d56a7699f8700df3c0137
martialmevongo <martial.mevongo@lapiscine.pro>
2025-04-14 12:32:34 +0200

    order

--- model/order.model.php
+++ model/order.model.php
@@ -4,12 +4,24 @@
 class order {
     //public sert a déclarer une propriété
     // j'ajoute la propriété product
-    public $product
+    public $product;
     // la proprié quantity
-    public $quantity
+    public $quantity;
     // la propriété createdA
-    public $createdAt
+    public $createdAt;
     // la propriéte status
-    public $status
-};
+    public $status;
+}
+
+
+//je recupère la call $order et je créé un objet que $order
+//En ajoutant chaque propriété de mon objet $order, je viens lui déclarer une valeur
+$order = new Order();
+
+$order->product = "Teeshirt Mario";
+$order->quantity = "1"
+$order->cratedAt = "new dateTime"
+$order->status = "CART"
+
+
 
